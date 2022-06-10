c26332ca792bd7611937a98e62d795a3d9b883dc
Deximus-Maximus <kev12east@gmail.com>
2022-06-09 20:23:37 -0400

    Make cache threadsafe

--- src/main/java/net/fabricmc/fabric/api/tag/client/v1/api/LocalTags.java
+++ src/main/java/net/fabricmc/fabric/api/tag/client/v1/api/LocalTags.java
@@ -4,17 +4,14 @@ import it.unimi.dsi.fastutil.objects.Object2ObjectOpenHashMap;
 import net.fabricmc.fabric.api.tag.client.v1.impl.DataLoader;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
 
 public class LocalTags {
     @ApiStatus.Internal
-    public static final Map<LocalTag, Set<Identifier>> LOCAL_TAG_CACHE = new Object2ObjectOpenHashMap<>();
+    public static final Map<LocalTag, Set<Identifier>> LOCAL_TAG_CACHE =
+            Collections.synchronizedMap(new Object2ObjectOpenHashMap<>());
     private static final DataLoader LOADER = new DataLoader();
 
     public static Set<Identifier> getOrCreateLocalTag(TagType type, Identifier tagId) {
