c53ca812291b2c3b54e71506d7258caffe78de5d
Deximus-Maximus <kev12east@gmail.com>
2022-06-09 23:07:41 -0400

    Allow RegistryEntries to be passed to TagUtil

--- src/main/java/net/fabricmc/fabric/api/tag/client/v1/api/TagUtil.java
+++ src/main/java/net/fabricmc/fabric/api/tag/client/v1/api/TagUtil.java
@@ -1,7 +1,13 @@
 package net.fabricmc.fabric.api.tag.client.v1.api;
 
+import net.fabricmc.fabric.api.tag.client.v1.test.ExampleMod;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.tag.TagKey;
 import net.minecraft.util.registry.Registry;
+import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryKey;
+
+import java.util.Optional;
 
 public class TagUtil {
     @SuppressWarnings("unchecked")
@@ -43,6 +49,58 @@ public class TagUtil {
                 var ids = LocalTags.getOrCreateLocalTag(tagKey);
                 return maybeKey.filter(registryKey -> ids.contains(registryKey.getValue())).isPresent();
             }
+        }
+
+        return false;
+    }
+
+    public static <T> boolean isIn(TagKey<T> tagKey, RegistryEntry<T> registryEntry) {
+        // Check if the tag exists in the dynamic registry first
+        if (MinecraftClient.getInstance() != null) {
+            if (MinecraftClient.getInstance().world != null) {
+                if (MinecraftClient.getInstance().world.getRegistryManager() != null) {
+                    Optional<? extends Registry<T>> maybeRegistry = MinecraftClient.getInstance().world
+                            .getRegistryManager().getOptional(tagKey.registry());
+                    if (maybeRegistry.isPresent()) {
+                        if (maybeRegistry.get().containsTag(tagKey)) {
+                            registryEntry.isIn(tagKey);
+                        }
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static <T> boolean isInWithLocalFallback(TagKey<T> tagKey, RegistryEntry<T> registryEntry) {
+        // Check if the tag exists in the dynamic registry first
+        if (MinecraftClient.getInstance() != null) {
+            if (MinecraftClient.getInstance().world != null) {
+                if (MinecraftClient.getInstance().world.getRegistryManager() != null) {
+                    Optional<? extends Registry<T>> maybeRegistry = MinecraftClient.getInstance().world
+                            .getRegistryManager().getOptional(tagKey.registry());
+                    if (maybeRegistry.isPresent()) {
+                        if (maybeRegistry.get().containsTag(tagKey)) {
+                            registryEntry.isIn(tagKey);
+                        }
+                    }
+                }
+            }
+        }
+
+        if (registryEntry.getKey().isPresent()) {
+            return isInLocal(tagKey, registryEntry.getKey().get());
+        }
+
+        return false;
+    }
+
+    public static <T> boolean isInLocal(TagKey<T> tagKey, RegistryKey<T> registryKey) {
+        if (tagKey.registry().getValue().equals(registryKey.getRegistry())) {
+            // Check local tags
+            var ids = LocalTags.getOrCreateLocalTag(tagKey);
+            return ids.contains(registryKey.getValue());
         }
 
         return false;
--- src/main/java/net/fabricmc/fabric/api/tag/client/v1/test/ExampleMod.java
+++ src/main/java/net/fabricmc/fabric/api/tag/client/v1/test/ExampleMod.java
@@ -3,8 +3,12 @@ package net.fabricmc.fabric.api.tag.client.v1.test;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.tag.client.v1.api.LocalTags;
 import net.fabricmc.fabric.api.tag.client.v1.api.TagUtil;
+import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBiomeTags;
 import net.fabricmc.fabric.api.tag.convention.v1.ConventionalBlockTags;
 import net.minecraft.block.Blocks;
+import net.minecraft.world.biome.BiomeKeys;
+import net.minecraft.world.biome.BuiltinBiomes;
+import net.minecraft.world.biome.source.BiomeSources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -23,8 +27,10 @@ public class ExampleMod implements ModInitializer {
 		LOGGER.info("Hello Fabric world!");
 
 		LOGGER.info(String.valueOf(LocalTags.getOrCreateLocalTag(ConventionalBlockTags.ORES)));
+		LOGGER.info(String.valueOf(LocalTags.getOrCreateLocalTag(ConventionalBiomeTags.AQUATIC)));
 
 		LOGGER.info(String.valueOf(TagUtil.isInWithLocalFallback(ConventionalBlockTags.ORES, Blocks.DIAMOND_ORE)));
+		LOGGER.info(String.valueOf(TagUtil.isInLocal(ConventionalBiomeTags.AQUATIC, BiomeKeys.OCEAN)));
 		LOGGER.info(String.valueOf(TagUtil.isInWithLocalFallback(ConventionalBlockTags.ORES, Blocks.DIAMOND_BLOCK)));
 		LOGGER.info(String.valueOf(TagUtil.isIn(ConventionalBlockTags.ORES, Blocks.DIAMOND_ORE)));
 		LOGGER.info(String.valueOf(TagUtil.isIn(ConventionalBlockTags.ORES, Blocks.DIAMOND_BLOCK)));
